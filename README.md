c78704c6bf2752af26b7ee806849cff370e5ee56
Brad Stancel <brad@processfast.com>
2018-06-12 15:31:31 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,70 @@
+setup-mysql-backups
+=========
+
+This Ansible role copies a bash script to the machine that uses mysqldump to backup one or more databases on a nightly cron job. It keeps the latest backup in a folder called `/backups/mysql/current` so that a nightly offsite backup copy/include that file into a backup system. It then moves non-current DB backups into the `/backups/mysql` folder and retains backups up to the number specified by the `num_db_backups_to_keep` variable (default of 30 backups to keep). 
+
+Requirements
+------------
+
+Some MySQL database(s) that you would like to backup on the server(s) you are running this against. MySQL and mysqldump installed on the server you are running this against.
+
+Role Variables
+--------------
+
+List of databases to backup
+
+`
+		dbs_to_backup: 
+		  - "business"
+`
+
+MySQL / MariaDB / Percona Server root password
+
+		`mysql_root_password: "your password here"``
+
+Number of DB Backups to keep
+
+		`num_db_backups_to_keep: 30`
+
+
+Dependencies
+------------
+
+None
+
+Example Playbook
+----------------
+
+Including an example of how to use your role (for instance, with variables passed in as parameters) is always nice for users too:
+
+	- hosts: your_server
+	  vars_files:
+	    - vars/main.yml
+	  roles:
+	    - { role: stancel.setup-mysql-backups }
+
+
+or 
+
+
+	- hosts: your_server 
+	  vars:
+		num_db_backups_to_keep: 30
+		mysql_root_password: "some password here"
+		dbs_to_backup: 
+          - 'db1'
+          - 'db2'
+	  roles:
+	    - { role: stancel.apache-webserver }
+
+
+License
+-------
+
+BSD
+
+Author Information
+------------------
+
+Brad Stancel
+
---
+++ templates/backup_mysql_databases.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+###################################################################
+#Script Name	backup_mysql_databases.sh                                                                                              
+#Description	Runs the mysqldump command to backup databases, move them to a folder and keep only a certain number                                                                               
+#Args           	None                                                                                           
+#Author       	Brad Stancel                                             
+#Email         	brad.stancel@gmail.com                                           
+###################################################################
+
+### MySQL Server Login Info ###
+MUSER="root"
+MPASS="{{ mysql_root_password }}"
+MHOST="localhost"
+MYSQL="$(which mysql)"
+MYSQLDUMP="$(which mysqldump)"
+DESTINATION="/backups/mysql"
+GZIP="$(which gzip)"
+declare -i NUM_BACKUPS_TO_KEEP={{ num_db_backups_to_keep }}
+NOW=$(date +"%m-%d-%Y")
+### Space separated list of all databases to backup
+DBS_TO_BACKUP={% for db in dbs_to_backup %}{{ db }}{% if not loop.last %} {% endif %}{% endfor %}
+
+### FTP SERVER Login info ###
+#FTPU="FTP-SERVER-USER-NAME"
+#FTPP="FTP-SERVER-PASSWORD"
+#FTPS="FTP-SERVER-IP-ADDRESS"
+#NOW=$(date +"%m-%d-%Y")
+ 
+### See comments below ###
+### [ ! -d $BAK ] && mkdir -p $BAK || /bin/rm -f $BAK/* ###
+[ ! -d ${DESTINATION} ] && mkdir -p ${DESTINATION}
+ 
+#$DBS="$($MYSQL -u $MUSER -h $MHOST -p$MPASS -Bse 'show databases')"
+#for db in $DBS
+for db in ${DBS_TO_BACKUP}
+do
+ FILE=${DESTINATION}/mysql-db-${db}.${NOW}-$(date +"%T").gz
+ $MYSQLDUMP --single-transaction -u $MUSER -h $MHOST -p$MPASS $db | $GZIP -9 > $FILE
+done
+
+cd ${DESTINATION}
+#num_files=`ls | wc -l`
+#rm `ls -tp | grep -v "/$" | awk '{ if (NR > "$NUM_BACKUPS_TO_KEEP") print; }'`
+
+declare -i NUM_TO_KEEP=$(($NUM_BACKUPS_TO_KEEP + 1))
+
+ls -t1 ${DESTINATION} | tail -n +$NUM_TO_KEEP | xargs rm -rf
+#lftp -u $FTPU,$FTPP -e "mkdir /mysql/$NOW;cd /mysql/$NOW; mput /backup/mysql/*; quit" $FTPS
+
+[ ! -d ${DESTINATION}/current ] && mkdir -p ${DESTINATION}/current
+
+rm -f ${DESTINATION}/current/*
+
+cp -p `ls -dtr1 ${DESTINATION}/*.gz | tail -1` ${DESTINATION}/current/
+
+### AFTER THIS IS SETUP A CRON JOB WITH THE BELOW COMMANDS
+# crontab -e
+### THEN PASTE THIS IN:
+## BRAD EDIT - Backing Up the MySQL DB Every Night
+#@midnight /root/backup_mysql_databases.sh >/dev/null 2>&1
+
+
+
+
